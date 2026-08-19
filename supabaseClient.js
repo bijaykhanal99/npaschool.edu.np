58ee2d4aa47965be71a0b2940d6eed9ca3110a69
Bijay Khanal <116628282+bijaykhanal99@users.noreply.github.com>
2026-08-19 19:19:44 +0545

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -4,6 +4,6 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://uvizctibsqyrghunkjnk.supabase.co'
 
 // यहाँ तपाईंको फोटोमा देखिएको Publishable Key वा Legacy Anon Key राख्नुहोस्
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV2aXpjdGlic3F5cmdodW5ram5rIiwicm9sZSI6ImFub24i'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV2aXpjdGlic3F5cmdodW5ram5rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcxNDIwNTEsImV4cCI6MjEwMjcxODA1MX0.nANHGa40Y8VfPu2KoOFTpbC2trvrGZ_g64qBzkOt800'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
